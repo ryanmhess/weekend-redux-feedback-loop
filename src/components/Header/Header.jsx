07f74d468ae2a06093431c28e221fdf947b53b06
Ryan Hess <ryanmhess@gmail.com>
2022-09-25 20:00:06 -0500

    all pages of feedback survey working, including post to server, clear of store and return to main - added toggle for dark theme - added speed dial for submitting a new feedback form, a link to a history page and an admin page - history and admin next on todos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 function Header() {
+
     return(
         <header className='App-header'>
-            <h1 className='App-title'>Feedback!</h1>
-            <h4>Don't forget it!</h4>
+                <h1 className='App-title'>Feedback:</h1>
+                <h4>Help us, help you!</h4>
         </header>
     )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,22 @@
-import { useHistory } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
 
-import Button from '@mui/material/Button';
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 
 function Main() {
 
     const history = useHistory();
 
+    const actions = [
+        { icon: <Link to='/feeling'><AssignmentIcon /></Link>, name: 'New Feedback' },
+        { icon: <Link to='/history'><InventoryIcon /></Link>, name: 'My History' },
+        { icon: <Link to='/admin'><AdminPanelSettingsIcon /></Link>, name: 'Admin' },
+    ];
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -21,19 +28,37 @@ function Main() {
     }
 
     return(
-        <Paper elevation={13} style={{maxWidth:650, margin:"0 auto", padding:"20px 5px"}}>
-            <form onSubmit={handleSubmit}>
-                <Grid container spacing={2}>
-                    <Grid xs={12} item>
-                    <Typography gutterBottom variant="h5">This is the main page</Typography>
-                    </Grid>
-                    <Grid xs={12} item>
-                        <Button type="submit" variant="contained" endIcon={<ArrowForwardIosRoundedIcon />}>
-                            NEW
-                        </Button>
-                    </Grid>
-                </Grid>
-            </form>
+        <Paper elevation={13} style={{maxWidth:450, margin:"0 auto", padding:"20px 0px"}} >
+            <SpeedDial ariaLabel="SpeedDial basic example" direction="down" sx={{ position: 'absolute', top: 125, left: 10 }} icon={<SpeedDialIcon />}>
+                {actions.map((action) => (
+                <SpeedDialAction key={action.name} icon={action.icon} tooltipTitle={action.name}/> ))}
+            </SpeedDial>      
+            <div className="row">
+                <div className="column">
+                    <img src="./images/mb.webp"/>
+                    <img src="./images/cl.webp"/>
+                    <img src="./images/ds.webp"/>
+                </div>
+                <div className="column">
+                    <img src="./images/bs.jpeg"/>
+                    <a>
+                        <img src="./images/goat_small.jpg"/>
+                        <img src="./images/cb.webp"/>
+                    </a>
+                    
+                    <img src="./images/mm.webp"/>
+                </div>
+                <div className="column">
+                    <img src="./images/es.jpeg"/>
+                    <img src="./images/es.webp"/>
+                    <img src="./images/ks.webp"/>
+                </div>
+                <div className="column">
+                    <img src="./images/lk.webp"/>
+                    <img src="./images/bk.webp"/>
+                    <img src="./images/mh.webp"/>
+                </div>
+            </div>
         </Paper>
     )
 }
